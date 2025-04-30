32a256082d6e647d20b3cbe5ffc3d50359bd7fd5
Aleksandra Vukicevic <doda@Aleksandras-MacBook-Pro.local>
2025-04-30 16:23:51 +0200

    Greeting

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args){
         //Disabling code from the previous lecture to focus on the current example
-        /*
+
         System.out.println("Hello World!");
         Greeting greeting = new Greeting();
         greeting.hello();
-         */
+         
 
         Messanger messanger = new Messanger();
         messanger.sendMessage();
