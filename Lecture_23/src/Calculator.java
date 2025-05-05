ff06eb0af0622fa428d70c2428ed26dc7d3e6bde
sonja <dragicevic.sonja@hotmail.com>
2025-05-05 15:31:45 +0200

    Lecture_23 - first Junit test

---
+++ Lecture_23/src/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public int add(int number1,int number2){
+        return number1 + number2;
+    }
+
+    public int subtrack(int number1, int number2){
+        return number1 - number2;
+    }
+}
---
+++ Lecture_23/test/CalculatorTests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTests {
+
+    @Test //kazemo Junitu da je ovaj metod test method i to radimo tako sto dodajemo @test
+    public void addTest(){
+        Calculator calculator = new Calculator();//napravili istancu
+        int result = calculator.add(5,3); //pozvali smo metodu add ali smo je dodelili varijabli result radi lakse manipulacije
+        Assertions.assertEquals(8,result); // sada proveravamo da li nam je kalkulacija tacna, prvo ide tacan odgovor, pa rezultat metode
+    }
+
+    @Test
+    public void subtrackTest(){
+        Calculator calculator = new Calculator();
+        int results1 = calculator.subtrack(8,3);
+        Assertions.assertEquals(5, results1);
+    }
+}
