692f4f1fc79249ee471a576bc71c75a153d00913
sonja <dragicevic.sonja@hotmail.com>
2025-05-19 10:46:27 +0200

    Lecture_23 - Calculatiore - first Junit test

--- Lecture_23/test/CalculatorTests.java
+++ Lecture_23/test/CalculatorTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 public class CalculatorTests {
 
-    @Test //kazemo Junitu da je ovaj metod test method i to radimo tako sto dodajemo @test
+    @Test //kazemo Junitu da je ovaj metod test method. to radimo tako sto dodajemo @test
     public void addTest(){
         Calculator calculator = new Calculator();//napravili istancu
         int result = calculator.add(5,3); //pozvali smo metodu add ali smo je dodelili varijabli result radi lakse manipulacije
@@ -13,7 +13,7 @@ public class CalculatorTests {
     @Test
     public void subtrackTest(){
         Calculator calculator = new Calculator();
-        int results1 = calculator.subtrack(8,3);
-        Assertions.assertEquals(5, results1);
+        int results1 = calculator.subtrack(5,3);
+        Assertions.assertEquals(2, results1);
     }
 }
